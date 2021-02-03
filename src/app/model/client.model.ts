a560bb41c633ea4b064f76a877f455354654623c
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-03 19:06:04 +0600

    client model created

---
+++ src/app/model/client.model.ts
@@ -0,0 +1,11 @@
+export interface Client{
+  id: number;
+  name: string;
+  nid: number;
+  address: string;
+  age: number;
+  gender: string;
+  email: string;
+  password: string;
+  balance: number;
+}
