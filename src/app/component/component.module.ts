b58dbfedf684efdce6475a561e045288fddb52e2
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-05 18:44:47 +0600

    cash-in c created

--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -4,12 +4,14 @@ import { FooterComponent } from './footer/footer.component';
 import {AppRoutingModule, RoutingComponents} from "../app-routing.module";
 import {MaterialModule} from "../material/material.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { CashInComponent } from './cash-in/cash-in.component';
 
 
 @NgModule({
   declarations: [
     FooterComponent,
     RoutingComponents,
+    CashInComponent,
   ],
   exports: [
     FooterComponent
