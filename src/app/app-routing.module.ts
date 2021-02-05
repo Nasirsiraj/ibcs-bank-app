151bfe0906a0b0b411a6e200640f3030ae9699a3
Nasir Siraj <mdnasir74725@gmail.com>
2021-02-05 18:51:47 +0600

    routing updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,12 +7,20 @@ import {ContactComponent} from "./component/contact/contact.component";
 import {HelpComponent} from "./component/help/help.component";
 import {ShareComponent} from "./component/share/share.component";
 import {SettingsComponent} from "./component/settings/settings.component";
+import {CheckBalanceComponent} from "./component/check-balance/check-balance.component";
+import {CashInComponent} from "./component/cash-in/cash-in.component";
+import {CashOutComponent} from "./component/cash-out/cash-out.component";
+import {SendMoneyComponent} from "./component/send-money/send-money.component";
 
 const routes: Routes = [
   {path: "", redirectTo: "dashboard", pathMatch: "full"},
   {path: "dashboard", component: DashboardComponent, children: [
       {path: '', redirectTo: 'create-account', pathMatch: 'full'},
       {path: 'create-account', component: CreateBankAccountComponent},
+      {path: 'check-balance', component: CheckBalanceComponent},
+      {path: 'cash-in', component: CashInComponent},
+      {path: 'cash-out', component: CashOutComponent},
+      {path: 'send-money', component: SendMoneyComponent},
       {path: 'contact', component: ContactComponent},
       {path: 'help', component: HelpComponent},
       {path: 'share', component: ShareComponent},
@@ -32,6 +40,10 @@ export const RoutingComponents = [
   DashboardComponent,
   ErrorPageComponent,
   CreateBankAccountComponent,
+  CheckBalanceComponent,
+  CashInComponent,
+  CashOutComponent,
+  SendMoneyComponent,
   ContactComponent,
   HelpComponent,
   ShareComponent,
--- src/app/component/component.module.ts
+++ src/app/component/component.module.ts
@@ -4,20 +4,12 @@ import { FooterComponent } from './footer/footer.component';
 import {AppRoutingModule, RoutingComponents} from "../app-routing.module";
 import {MaterialModule} from "../material/material.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import { CashInComponent } from './cash-in/cash-in.component';
-import { CashOutComponent } from './cash-out/cash-out.component';
-import { SendMoneyComponent } from './send-money/send-money.component';
-import { CheckBalanceComponent } from './check-balance/check-balance.component';
 
 
 @NgModule({
   declarations: [
     FooterComponent,
-    RoutingComponents,
-    CashInComponent,
-    CashOutComponent,
-    SendMoneyComponent,
-    CheckBalanceComponent,
+    RoutingComponents
   ],
   exports: [
     FooterComponent
